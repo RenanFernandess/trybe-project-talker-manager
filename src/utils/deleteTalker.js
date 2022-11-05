cf7550b712b6e0a445700f315f27ee3dfc1e3023
Renan Fernandes <renzinestuods@gmail.com>
2022-11-05 12:31:42 -0300

    :sparkles: feat: Cria a função deleteTalker que remove um palestrante da lista

---
+++ src/utils/deleteTalker.js
@@ -0,0 +1,11 @@
+const getTalker = require('./getTalker');
+
+const saveTalkerList = require('./saveTalkerList');
+
+const deleteTalker = async (talkerId) => {
+  const talkerList = await getTalker();
+  const newList = talkerList.filter(({ id }) => id !== talkerId);
+  saveTalkerList(newList);
+};
+
+module.exports = deleteTalker;
