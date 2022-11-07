4878c9b17bcf7a5eddfe3f3ee626ab8c27c0c7c6
Renan Fernandes <renzinestuods@gmail.com>
2022-11-07 16:22:44 -0300

    :bug: fix: Corrige o bug que estava fazendo ser gerado um token com mias de 16 caracteres

--- src/utils/tokenGenerator.js
+++ src/utils/tokenGenerator.js
@@ -1,12 +1,12 @@
 const CHARACTERS = 'ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklnmopqrstuvwxyz0123456789';
 
-const generateRandomIndex = () => Math.round(Math.random() * CHARACTERS.length - 1);
+const generateRandomIndex = () => Math.round(Math.random() * (CHARACTERS.length - 1));
 
 const tokenGenerator = () => {
   const TOKEN_NUMBER_CHARACTERS = 16;
-  const TOKEN = Array(TOKEN_NUMBER_CHARACTERS).fill('')
+  return Array(TOKEN_NUMBER_CHARACTERS)
+    .fill('')
     .reduce((acc) => `${acc}${CHARACTERS[generateRandomIndex()]}`, '');
-  return TOKEN;
 };
 
 module.exports = {
