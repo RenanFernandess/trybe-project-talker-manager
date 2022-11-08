7c9f1267b3fd5302a88c755ba8ca74e5e264c785
Renan Fernandes <renzinestuods@gmail.com>
2022-11-08 11:55:47 -0300

    :bug: fix: Corrige o proble que estava quebrando o codigo, a messagem errada e o if que não estava executando

--- src/middlewares/talkValidate.js
+++ src/middlewares/talkValidate.js
@@ -1,15 +1,17 @@
-const DATE_REGEX = /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[012])\/\d{4}$/ig;
+const DATE_REGEX = /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[012])\/\d{4}$/;
 
-const talkValidate = ({ body: { talk: { watchedAt, rate }, talk } }, res, next) => {
+const talkValidate = ({ body: { talk } }, res, next) => {
   if (!talk) return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
 
+  const { watchedAt, rate } = talk;
+
   if (!watchedAt) return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
-  if (DATE_REGEX.test(watchedAt)) {
+  if (!DATE_REGEX.test(watchedAt)) {
     return res.status(400)
       .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }
 
-  if (!rate) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (!rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
 
   next();
 };
