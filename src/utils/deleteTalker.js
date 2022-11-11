64b3f7f36e5556d5265ac12be24ec5fdc83ddcc5
Renan Fernandes <renzinestuods@gmail.com>
2022-11-11 17:48:50 -0300

    :recycle: refactor: Espera a função saveTalkerList

--- src/utils/deleteTalker.js
+++ src/utils/deleteTalker.js
@@ -5,7 +5,7 @@ const saveTalkerList = require('./saveTalkerList');
 const deleteTalker = async (talkerId) => {
   const talkerList = await getTalker();
   const newList = talkerList.filter(({ id }) => id !== talkerId);
-  saveTalkerList(newList);
+  await saveTalkerList(newList);
 };
 
 module.exports = deleteTalker;
